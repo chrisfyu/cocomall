8ef433534559c9ef252c5ad57afca8ce4add789f
yufei <chrisfyu@hotmail.com>
2026-02-04 07:12:11 +0900

    add showing status of brand

--- renren-fast-vue/src/views/modules/product/brand.vue
+++ renren-fast-vue/src/views/modules/product/brand.vue
@@ -50,7 +50,17 @@
         prop="showStatus"
         header-align="center"
         align="center"
-        label="显示状态[0-不显示；1-显示]">
+        label="显示状态">
+        <template slot-scope="scope">
+          <el-switch
+            v-model="scope.row.showStatus"
+            active-color="#13ce66"
+            inactive-color="#ff4949"
+            :active-value="1"
+            :inactive-value="0"
+            @change="updateBrandStatus(scope.row)"
+          ></el-switch>
+        </template>
       </el-table-column>
       <el-table-column
         prop="firstLetter"
@@ -135,6 +145,21 @@
           }
           this.dataListLoading = false
         })
+      },
+      updateBrandStatus(data) {
+        console.log("最新信息", data);
+        let { brandId, showStatus } = data;
+        //发送请求修改状态
+        this.$http({
+          url: this.$http.adornUrl("/product/brand/update"),
+          method: "post",
+          data: this.$http.adornData({ brandId, showStatus: showStatus? 1 : 0 }, false)
+        }).then(({ data }) => {
+          this.$message({
+            type: "success",
+            message: "状态更新成功"
+          });
+        });
       },
       // 每页数
       sizeChangeHandle (val) {
