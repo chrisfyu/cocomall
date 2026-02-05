8ef782dbdb08fdb37b7d21d5cdebdf71325a570d
yufei <chrisfyu@hotmail.com>
2026-02-05 22:02:37 +0900

    add validation by group and customized validator

--- cocomall-product/src/main/java/com/atguigu/cocomall/product/controller/BrandController.java
+++ cocomall-product/src/main/java/com/atguigu/cocomall/product/controller/BrandController.java
@@ -4,8 +4,12 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.atguigu.common.valid.AddGroup;
+import com.atguigu.common.valid.UpdateGroup;
+import com.atguigu.common.valid.UpdateStatusGroup;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +20,6 @@ import com.atguigu.cocomall.product.entity.BrandEntity;
 import com.atguigu.cocomall.product.service.BrandService;
 import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.R;
-
-import javax.validation.Valid;
-
 
 /**
  * 品牌
@@ -61,23 +62,25 @@ public class BrandController {
      */
     @RequestMapping("/save")
     //@RequiresPermissions("product:brand:save")
-    public R save(@Valid @RequestBody BrandEntity brand, BindingResult result){
+    public R save(@Validated({AddGroup.class}) @RequestBody BrandEntity brand
+            //, BindingResult result
+    ){
 
-         Map<String,String> map = new HashMap<>();
-         if (result.hasErrors()) {
-             //获取效验错误结果
-             result.getFieldErrors().forEach((item)-> {
-                 //获取到错误提示
-                 String message = item.getDefaultMessage();
-                 //获取错误的属性的名字
-                 String field = item.getField();
-                 map.put(field,message);
-             });
-             return R.error(400,"提交的数据不合法").put("data",map);
-         } else {
-             brandService.save(brand);
-         }
+//         Map<String,String> map = new HashMap<>();
+//         if (result.hasErrors()) {
+//             //获取效验错误结果
+//             result.getFieldErrors().forEach((item)-> {
+//                 //获取到错误提示
+//                 String message = item.getDefaultMessage();
+//                 //获取错误的属性的名字
+//                 String field = item.getField();
+//                 map.put(field,message);
+//             });
+//             return R.error(400,"提交的数据不合法").put("data",map);
+//         } else {
+//         }
 
+        brandService.save(brand);
         return R.ok();
     }
 
@@ -86,8 +89,19 @@ public class BrandController {
      */
     @RequestMapping("/update")
     //@RequiresPermissions("product:brand:update")
-    public R update(@RequestBody BrandEntity brand){
+    public R update(@Validated({UpdateGroup.class}) @RequestBody BrandEntity brand){
 		brandService.updateById(brand);
+
+        return R.ok();
+    }
+
+    /**
+     * 修改状态
+     */
+    @RequestMapping("/update/status")
+    //@RequiresPermissions("product:brand:update")
+    public R updateStatus(@Validated(UpdateStatusGroup.class) @RequestBody BrandEntity brand){
+        brandService.updateById(brand);
 
         return R.ok();
     }
--- renren-fast-vue/src/views/modules/product/brand.vue
+++ renren-fast-vue/src/views/modules/product/brand.vue
@@ -154,7 +154,7 @@
         let { brandId, showStatus } = data;
         //发送请求修改状态
         this.$http({
-          url: this.$http.adornUrl("/product/brand/update"),
+          url: this.$http.adornUrl("/product/brand/update/status"),
           method: "post",
           data: this.$http.adornData({ brandId, showStatus: showStatus? 1 : 0 }, false)
         }).then(({ data }) => {
