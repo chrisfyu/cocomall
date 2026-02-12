0991e55ff8695cb29b42dfe76fe1ba4a6af45cf1
yufei <chrisfyu@hotmail.com>
2026-02-12 19:07:14 +0900

    implement searching ware house info

--- cocomall-ware/src/main/java/com/atguigu/cocomall/ware/CocomallWareApplication.java
+++ cocomall-ware/src/main/java/com/atguigu/cocomall/ware/CocomallWareApplication.java
@@ -1,8 +1,14 @@
 package com.atguigu.cocomall.ware;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
+@MapperScan("com.atguigu.cocomall.ware.dao")
+@EnableFeignClients
 @SpringBootApplication
 public class CocomallWareApplication {
 
