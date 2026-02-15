6c6197baf958c87cf9320720f7ec421e057baacf
yufei <chrisfyu@hotmail.com>
2026-02-15 23:17:58 +0900

    delete oss secret info

--- cocomall-third-party/src/test/java/com/atguigu/cocomall/thirdparty/CocomallThirdPartyApplicationTests.java
+++ cocomall-third-party/src/test/java/com/atguigu/cocomall/thirdparty/CocomallThirdPartyApplicationTests.java
@@ -28,8 +28,8 @@ public class CocomallThirdPartyApplicationTests {
 //        // Endpoint以东京为例，其它Region请按实际情况填写。
 //        String endpoint = "oss-ap-northeast-1.aliyuncs.com";
 //        // 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-//        String accessKeyId = "LTAI5t9cbsLmdV2N47ov4ufH";
-//        String accessKeySecret = "ByzZq1NeTfcySDOMcEZaHT7NFyvuKk";
+//        String accessKeyId = "L**********H";
+//        String accessKeySecret = "B*********************k";
 //
 //        // 创建OSSClient实例。
 //        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
