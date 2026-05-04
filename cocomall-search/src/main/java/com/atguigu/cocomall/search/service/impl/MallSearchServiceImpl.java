a6fbf19492a8679932c410ecbe3e35edb7b5b94d
yufei <chrisfyu@hotmail.com>
2026-05-04 19:11:33 -0400

    implement ES searching result

--- cocomall-search/src/main/java/com/atguigu/cocomall/search/service/impl/MallSearchServiceImpl.java
+++ cocomall-search/src/main/java/com/atguigu/cocomall/search/service/impl/MallSearchServiceImpl.java
@@ -1,10 +1,12 @@
 package com.atguigu.cocomall.search.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.atguigu.cocomall.search.config.CocomallElasticSearchConfig;
 import com.atguigu.cocomall.search.constant.EsConstant;
 import com.atguigu.cocomall.search.service.MallSearchService;
 import com.atguigu.cocomall.search.vo.SearchParam;
 import com.atguigu.cocomall.search.vo.SearchResult;
+import com.atguigu.common.es.SkuEsModel;
 import org.apache.lucene.search.join.ScoreMode;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
@@ -13,17 +15,27 @@ import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.NestedQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.aggregations.bucket.nested.NestedAggregationBuilder;
+import org.elasticsearch.search.aggregations.bucket.nested.ParsedNested;
+import org.elasticsearch.search.aggregations.bucket.terms.ParsedLongTerms;
+import org.elasticsearch.search.aggregations.bucket.terms.ParsedStringTerms;
+import org.elasticsearch.search.aggregations.bucket.terms.Terms;
 import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @Description:
@@ -46,7 +58,7 @@ public class MallSearchServiceImpl implements MallSearchService {
         try {
             SearchResponse response = client.search(searchRequest, CocomallElasticSearchConfig.COMMON_OPTIONS);
 
-            result = buildSearchResult(response);
+            result = buildSearchResult(response, param);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -182,9 +194,103 @@ public class MallSearchServiceImpl implements MallSearchService {
     /**
      * 构建结果数据
      * @param response
+     * @param param
      * @return
      */
-    private SearchResult buildSearchResult(SearchResponse response) {
-        return null;
+    private SearchResult buildSearchResult(SearchResponse response, SearchParam param) {
+
+        SearchResult result = new SearchResult();
+
+        //1、返回的所有查询到的商品
+        SearchHits hits = response.getHits();
+        List<SkuEsModel> esModels = new ArrayList<>();
+        if (hits.getHits() != null && hits.getHits().length > 0) {
+            for (SearchHit hit : hits.getHits()) {
+                String sourceAsString = hit.getSourceAsString();
+                SkuEsModel esModel = JSON.parseObject(sourceAsString, SkuEsModel.class);
+                if (!StringUtils.isEmpty(param.getKeyword())) {
+                    HighlightField skuTitle = hit.getHighlightFields().get("skuTitle");
+                    String s = skuTitle.getFragments()[0].string();
+                    esModel.setSkuTitle(s);
+                }
+
+                esModels.add(esModel);
+            }
+
+        }
+        result.setProducts(esModels);
+
+        //2、当前商品涉及到的所有属性信息
+        List<SearchResult.AttrVo> attrVos = new ArrayList<>();
+        ParsedNested attr_agg = response.getAggregations().get("attr_agg");
+        ParsedLongTerms attr_id_agg = attr_agg.getAggregations().get("attr_id_agg");
+
+        for (Terms.Bucket bucket : attr_id_agg.getBuckets()) {
+            SearchResult.AttrVo attrVo = new SearchResult.AttrVo();
+
+            long attrId = bucket.getKeyAsNumber().longValue();
+
+            String attrName = ((ParsedStringTerms) bucket.getAggregations().get("attr_name_agg")).getBuckets().get(0).getKeyAsString();
+
+            List<String> attrValues = ((ParsedStringTerms) bucket.getAggregations().get("attr_value_agg")).getBuckets().stream().map(item -> {
+                String keyAsString = ((Terms.Bucket) item).getKeyAsString();
+                return keyAsString;
+            }).collect(Collectors.toList());
+
+            attrVo.setAttrId(attrId);
+            attrVo.setAttrName(attrName);
+            attrVo.setAttrValue(attrValues);
+            attrVos.add(attrVo);
+        }
+        result.setAttrs(attrVos);
+
+        //3、当前商品涉及到的所有品牌信息
+        List<SearchResult.BrandVo> brandVos = new ArrayList<>();
+        ParsedLongTerms brand_agg = response.getAggregations().get("brand_agg");
+
+        for (Terms.Bucket bucket : brand_agg.getBuckets()) {
+            SearchResult.BrandVo brandVo = new SearchResult.BrandVo();
+
+            long brandId = bucket.getKeyAsNumber().longValue();
+
+            String brandName = ((ParsedStringTerms) bucket.getAggregations().get("brand_name_agg")).getBuckets().get(0).getKeyAsString();
+            String brandImg = ((ParsedStringTerms) bucket.getAggregations().get("brand_img_agg")).getBuckets().get(0).getKeyAsString();
+
+            brandVo.setBrandId(brandId);
+            brandVo.setBrandName(brandName);
+            brandVo.setBrandImg(brandImg);
+            brandVos.add(brandVo);
+        }
+
+        result.setBrands(brandVos);
+
+        //4、当前商品涉及到的所有分类信息
+        ParsedLongTerms catalog_agg = response.getAggregations().get("catalog_agg");
+        List<SearchResult.CatalogVo> catalogVos = new ArrayList<>();
+        List<? extends Terms.Bucket> buckets = catalog_agg.getBuckets();
+        for (Terms.Bucket bucket : buckets) {
+            SearchResult.CatalogVo catalogVo = new SearchResult.CatalogVo();
+
+            String keyAsString = bucket.getKeyAsString();
+            catalogVo.setCatalogId(Long.parseLong(keyAsString));
+
+            ParsedStringTerms catalog_name_agg = bucket.getAggregations().get("catalog_name_agg");
+            String catalog_name = catalog_name_agg.getBuckets().get(0).getKeyAsString();
+            catalogVo.setCatalogName(catalog_name);
+
+            catalogVos.add(catalogVo);
+        }
+        result.setCatalogs(catalogVos);
+
+        //5、分页信息-页码
+        result.setPageNum(param.getPageNum());
+
+        long total = hits.getTotalHits().value;
+        result.setTotal(total);
+
+        int totalPages = (int)total % EsConstant.PRODUCT_PAGESIZE == 0? (int)total / EsConstant.PRODUCT_PAGESIZE : ((int)total / EsConstant.PRODUCT_PAGESIZE + 1);
+        result.setTotalPages(totalPages);
+        //        result.setProducts();
+        return result;
     }
 }
