7bd48a35595d6a6fc157255ae609a8e6f3d64256
yufei <chrisfyu@hotmail.com>
2026-05-12 22:02:25 -0400

    add auth-server module and pages

---
+++ cocomall-auth-server/src/main/java/com/atguigu/cocomall/auth/CocomallAuthServerApplication.java
@@ -0,0 +1,17 @@
+package com.atguigu.cocomall.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CocomallAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocomallAuthServerApplication.class, args);
+    }
+
+}
---
+++ cocomall-auth-server/src/test/java/com/atguigu/cocomall/auth/CocomallAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.cocomall.auth;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocomallAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
