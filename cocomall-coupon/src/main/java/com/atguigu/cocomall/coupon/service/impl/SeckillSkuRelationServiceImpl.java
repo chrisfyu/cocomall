7812710f25a1bb6cd06c55060c470da5a7d10a32
yufei <chrisfyu@hotmail.com>
2026-05-31 11:25:35 -0400

    create seckill module

--- cocomall-coupon/src/main/java/com/atguigu/cocomall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
+++ cocomall-coupon/src/main/java/com/atguigu/cocomall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
@@ -11,6 +11,7 @@ import com.atguigu.common.utils.Query;
 import com.atguigu.cocomall.coupon.dao.SeckillSkuRelationDao;
 import com.atguigu.cocomall.coupon.entity.SeckillSkuRelationEntity;
 import com.atguigu.cocomall.coupon.service.SeckillSkuRelationService;
+import org.springframework.util.StringUtils;
 
 
 @Service("seckillSkuRelationService")
@@ -18,9 +19,17 @@ public class SeckillSkuRelationServiceImpl extends ServiceImpl<SeckillSkuRelatio
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        QueryWrapper<SeckillSkuRelationEntity> queryWrapper = new QueryWrapper<>();
+
+        String promotionSessionId = (String) params.get("promotionSessionId");
+        if (!StringUtils.isEmpty(promotionSessionId)) {
+            queryWrapper.eq("promotion_session_id", promotionSessionId);
+        }
+
         IPage<SeckillSkuRelationEntity> page = this.page(
                 new Query<SeckillSkuRelationEntity>().getPage(params),
-                new QueryWrapper<SeckillSkuRelationEntity>()
+                queryWrapper
         );
 
         return new PageUtils(page);
---
+++ cocomall-seckill/src/test/java/com/atguigu/cocomall/lseckill/CocomallSeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.cocomall.lseckill;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocomallSeckillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
