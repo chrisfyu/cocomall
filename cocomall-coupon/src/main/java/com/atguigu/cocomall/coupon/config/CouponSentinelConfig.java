a961c91c725067d04b2761432a46a4ee3552b526
yufei <chrisfyu@hotmail.com>
2026-06-02 16:35:28 -0400

    add sentinel config to services

---
+++ cocomall-coupon/src/main/java/com/atguigu/cocomall/coupon/config/CouponSentinelConfig.java
@@ -0,0 +1,35 @@
+package com.atguigu.cocomall.coupon.config;
+
+import com.alibaba.csp.sentinel.adapter.servlet.callback.UrlBlockHandler;
+import com.alibaba.csp.sentinel.adapter.servlet.callback.WebCallbackManager;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.fastjson.JSON;
+import com.atguigu.common.exception.BizCodeEnume;
+import com.atguigu.common.utils.R;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Description:
+ * @Author: Fei Yu
+ * @CreateTime: 2026/6/2 12:11
+ */
+
+@Configuration
+public class CouponSentinelConfig {
+
+    public CouponSentinelConfig() {
+        WebCallbackManager.setUrlBlockHandler(new UrlBlockHandler() {
+            @Override
+            public void blocked(HttpServletRequest request, HttpServletResponse response, BlockException e) throws IOException {
+                R error = R.error(BizCodeEnume.TOO_MANY_REQUEST.getCode(), BizCodeEnume.TOO_MANY_REQUEST.getMsg());
+                response.setCharacterEncoding("UTF-8");
+                response.setContentType("application/json");
+                response.getWriter().write(JSON.toJSONString(error));
+            }
+        });
+    }
+}
