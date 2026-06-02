7f905a413f2654ea2fc1972b3d189f4f33b807bb
yufei <chrisfyu@hotmail.com>
2026-06-01 23:52:17 -0400

    add seckill success page

--- cocomall-seckill/src/main/java/com/atguigu/cocomall/seckill/controller/SeckillController.java
+++ cocomall-seckill/src/main/java/com/atguigu/cocomall/seckill/controller/SeckillController.java
@@ -4,10 +4,12 @@ import com.atguigu.cocomall.seckill.service.SeckillService;
 import com.atguigu.cocomall.seckill.to.SeckillSkuRedisTo;
 import com.atguigu.common.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -17,7 +19,7 @@ import java.util.List;
  * @CreateTime: 2026/5/31 22:03
  */
 
-@RestController
+@Controller
 public class SeckillController {
 
     @Autowired
@@ -27,12 +29,14 @@ public class SeckillController {
      * 返回当前时间可以参与的秒杀商品信息
      * @return
      */
+    @ResponseBody
     @GetMapping("/currentSeckillSkus")
     public R getCurrentSeckillSkus() {
         List<SeckillSkuRedisTo> vos = seckillService.getCurrentSeckillSkus();
         return R.ok().setData(vos);
     }
 
+    @ResponseBody
     @GetMapping("/sku/seckill/{skuId}")
     public R getSkuSeckillInfo(@PathVariable("skuId") Long skuId) {
         SeckillSkuRedisTo to = seckillService.getSkuSeckillInfo(skuId);
@@ -40,11 +44,14 @@ public class SeckillController {
     }
 
     @GetMapping("/kill")
-    public R seckill(@RequestParam("killId") String killId,
-                     @RequestParam("key") String key,
-                     @RequestParam("num") Integer num) {
+    public String seckill(@RequestParam("killId") String killId,
+                          @RequestParam("key") String key,
+                          @RequestParam("num") Integer num,
+                          Model model) {
 
         String orderSn = seckillService.kill(killId, key, num);
-        return R.ok().setData(orderSn);
+
+        model.addAttribute("orderSn", orderSn);
+        return "success";
     }
 }
